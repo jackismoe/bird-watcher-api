74cef6eaf7446af2ee7af527d19553880b53f974
jackismoe <jacksonmo@me.com>
2020-10-05 09:48:18 -0400

    Add validation of object type in process function

--- frontend/index.js
+++ frontend/index.js
@@ -1,37 +1,66 @@
+fetchUrl = 'http://localhost:3000/sightings'
+
+let body = document.querySelector('body')
+
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('http://localhost:3000/sightings')
+  fetch(fetchUrl)
     .then(response => response.json())
     .then(jsonResponse => process(jsonResponse))
 })
 
+const process = (object) => {
+  if (object instanceof Array) {
+    for (let x in object) {
+      let container = document.createElement('div')
+      let birdNameEl = document.createElement('h2')
+      let birdSpeciesEl = document.createElement('h3')
+      let birdLocation = document.createElement('p')
 
-const process = (arrayOfObjects) => {
-  for (let x in arrayOfObjects) {
-    let container = document.createElement('div')
-    let birdNameEl = document.createElement('h2')
-    let birdSpeciesEl = document.createElement('h3')
-    let birdLocation = document.createElement('p')
+      container.id = 'container'
+      birdNameEl.id = 'bird-name'
+      birdSpeciesEl.id = 'bird-species'
+      birdLocation.id = 'last-seen'
 
-    container.id = 'container'
-    birdNameEl.id = 'bird-name'
-    birdSpeciesEl.id = 'bird-species'
-    birdLocation.id = 'last-seen'
+      let newBird = object[x]
+      let birdName = newBird.bird.name
+      let birdSpecies = newBird.bird.species
+      let birdLat = newBird.location.latitude
+      let birdLong = newBird.location.longitude
 
-    let newBird = arrayOfObjects[x]
-    let birdName = newBird.bird.name
-    let birdSpecies = newBird.bird.species
-    let birdLat = newBird.location.latitude
-    let birdLong = newBird.location.longitude
+      birdNameEl.innerText = `Name: ${birdName}`
+      birdSpeciesEl.innerText = `Species: ${birdSpecies}`
+      birdLocation.innerText = `Last seen at ${birdLat}, ${birdLong}`
 
-    birdNameEl.innerText = `Name: ${birdName}`
-    birdSpeciesEl.innerText = `Species: ${birdSpecies}`
-    birdLocation.innerText = `Last seen at ${birdLat}, ${birdLong}`
+      container.appendChild(birdNameEl)
+      container.appendChild(birdSpeciesEl)
+      container.appendChild(birdLocation)
+      body.appendChild(container)
+    }
+  } else {
+     let container = document.createElement("div");
+     let birdNameEl = document.createElement("h2");
+     let birdSpeciesEl = document.createElement("h3");
+     let birdLocation = document.createElement("p");
 
-    container.appendChild(birdNameEl)
-    container.appendChild(birdSpeciesEl)
-    container.appendChild(birdLocation)
-    body.appendChild(container)
+     container.id = "container";
+     birdNameEl.id = "bird-name";
+     birdSpeciesEl.id = "bird-species";
+     birdLocation.id = "last-seen";
+
+     let birdName = object.bird.name;
+     let birdSpecies = object.bird.species;
+     let birdLat = object.location.latitude;
+     let birdLong = object.location.longitude;
+
+     birdNameEl.innerText = `Name: ${birdName}`;
+     birdSpeciesEl.innerText = `Species: ${birdSpecies}`;
+     birdLocation.innerText = `Last seen at ${birdLat}, ${birdLong}`;
+
+     container.appendChild(birdNameEl);
+     container.appendChild(birdSpeciesEl);
+     container.appendChild(birdLocation);
+     body.appendChild(container);
   }
 }
 
-let body = document.querySelector('body')
+// need to add functionality for showing individual sightings; dropdown menu
